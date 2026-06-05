541ac6ba99d43e8a6b1b7f44a0ee3a8b91bfcac7
Manibalan <manibalan290@gmail.com>
2026-06-05 11:17:13 +0530

    date filter change in doctor approval

--- core/views.py
+++ core/views.py
@@ -87,8 +87,8 @@ def get_investigations(request):
                     start_of_day = timezone.make_aware(start_of_day)
                     end_of_day = timezone.make_aware(end_of_day)
                     
-                billings = Billing.objects.filter(date__gte=start_of_day, date__lte=end_of_day)
-                barcodes = list(billings.values_list('barcode', flat=True))
+                employees = EmployeeRegistration.objects.filter(created_date__gte=start_of_day, created_date__lte=end_of_day)
+                barcodes = list(employees.values_list('barcode', flat=True))
                 query["barcode"] = {"$in": barcodes}
             except Exception as e:
                 logger.warning(f"Date parsing error in get_investigations (views.py): {e}")
